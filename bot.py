3ee048f434adff954d417cb7243e18e6bd7b79cd
Your Name <you@example.com>
2022-01-17 10:45:11 -0500

    Bot now sends message via Python script

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import slack
 import os
 from pathlib import Path
-from dotenv import load_env
+import dotenv
 
 env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
+dotenv.load_dotenv(dotenv_path=env_path)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-
-## another git commit test
+# client.chat_postMessage(channel="#slack-bot", text="Hello World!")
+client.chat_postMessage(channel="#general", text="Hello World!")
